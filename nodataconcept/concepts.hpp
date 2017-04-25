e9a07ad5b8bbb87c6b4b37452c45a30891cfb8c4
Nelson Gomez <ng2573@columbia.edu>
2017-04-25 01:53:01 -0700

    Add Serializable concept, derive Regression from it

--- nodataconcept/concepts.hpp
+++ nodataconcept/concepts.hpp
@@ -1,8 +1,17 @@
 #ifndef CONCEPTS_HPP
 #define CONCEPTS_HPP
 
+#include <iostream>
+
+template<typename T>
+concept bool Serializable = requires(T t, std::istream& i, std::ostream& o)
+{
+	{ i >> t } -> std::istream&;
+	{ i << t } -> std::ostream&;
+};
+
 template<typename T, typename D, typename L>
-concept bool Regression = requires(T t, D d, L l)
+concept bool Regression = Serializable<T> && requires(T t, D d, L l)
 {
 	t.train(d,l);
 	t.regress(d);
